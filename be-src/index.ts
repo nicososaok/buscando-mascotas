67424a694eabb8fe781799495540a389b1e4be05
Nicolas Sosa <nisososa62@gmail.com>
2026-06-10 00:51:44 -0300

    Fix: apply definitive strict type bypasses for algolia and state functions

--- be-src/index.ts
+++ be-src/index.ts
@@ -164,8 +164,8 @@ const distPath = path.resolve(__dirname, "../dist");
 
 app.use(express.static(distPath));
 
-app.get("*", (req, res) => {
-   res.sendFile(path.join(distPath, "index.html"));
+app.get(/\*/, (req, res) => {
+   res.sendFile(path.resolve(__dirname, "../dist/index.html"));
 });
 
 async function main() {
