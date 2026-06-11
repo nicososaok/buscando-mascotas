485d1da50d499fdbe914436b40a2be3924ace741
Nicolas Sosa <nisososa62@gmail.com>
2026-06-10 22:52:54 -0300

    Fix: backend bundled successfully into a single ESM file using tsup

--- be-src/index.ts
+++ be-src/index.ts
@@ -13,6 +13,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const SECRET = process.env.JWT_SECRET;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(json({ limit: "50mb" }));
 app.use(cors());
 
